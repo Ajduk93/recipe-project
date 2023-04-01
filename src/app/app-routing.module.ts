e536d554a5dca858c69eabf8189689cbf291883f
Ivan Ajdukovic <ajdukivan93@gmail.com>
2023-04-01 10:35:29 +0200

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavigationComponent } from './navigation/navigation.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Routes = [
-  {path:`/`, component:NavigationComponent},
   {path:`login`, component:LoginComponent},
   {path:`signup`,component:SignupComponent}
 ];
